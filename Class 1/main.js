7c51625a905fc0609e1783703b33f46e6c81274c
srivastava-vishal <visha>
2017-07-26 16:06:10 +0530

    updated new files

--- Class 1/main.js
+++ Class 1/main.js
@@ -77,9 +77,9 @@ foodieApp.controller('restaurantController',function($scope,$routeParams,$http) 
 	image: 'https://b.zmtcdn.com/data/pictures/chains/2/308022/dabd30bd0b000ea859ada9a08a0132fc.jpg'
 },
 {
-	name: 'Dominos',
-	address: '12/24, Level 1, Block A , Big Bazzar, Baddi',
-	location: 'Baddi',
+	name: 'dominos',
+	address: 'sector-18, Noida',
+	location: 'Noida',
 	category: 'Pizza',
 	vote: '4.7',
 	cuisines: 'Italian',
@@ -90,48 +90,49 @@ foodieApp.controller('restaurantController',function($scope,$routeParams,$http) 
 	name: 'Corn Pizza',
 	image: 'http://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG'
            },
-	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt_2XFEpZvUI_wAMAqEnnWoVr61jAej6k4VgzwRr-yONk2Es-h'
+	image: 'http://www.dominos.co.in/blog/wp-content/uploads/2011/08/03.jpg'
 },
 {
-	name: 'Silver Spoon Restaurant & Banquet Hall barnala',
-	address: 'White Wave Building, Opposite DC Complex, Handiaya Road, Barnala, Punjab 148101',
-	location: 'Connaught Place',
+	name: 'cheeze box',
+	address: 'near railway road ,kalka,haryana',
+	location: 'kalka',
 	category: 'Family Restaurant',
-	vote: '3.9',
+	vote: '4.0',
 	cuisines: 'Indian',
 	cost: '1000',
 	hours: '8 AM to 11 PM (Mon-Sun)',
  id:3,
-	image: 'http://lovethiscitytv.com/wp-content/uploads/2015/06/Top-25-New-Restaurants-in-Toronto2.jpeg'
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyD5HfeyitS3nIuzFsyrdpmLqYNckNrSaMJKWnonVvfbVoAW58MQ'
 },
 {
-	name: 'Pappu Dhaba',
-	address: 'Old City, Bathinda, Punjab 151001',
-	location: 'Bathinda',
+	name: 'saini Dhaba',
+	address: 'Old City,near ram leela ground,nalagarh',
+	location: 'nalagarh',
 	category: 'Casual Dining',
-	vote: '4.3',
+	vote: '2.5',
 	cuisines: 'Indian',
-	cost: '400',
+	cost: '300',
 	hours: '10 AM to 12 AM (Mon-Sun)',
  id :4,
-	image: 'http://restaurant.business.brookes.ac.uk/images/slideshow/restaurant.jpg'
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFwZIQWY58NKGch1erq5Z82I_dnUzxeQgwAu63fjHXN1PUSW5VWw'
 },
 {
-	name: 'Pizza Nation',
-	address: ' Shop No. 25 A, City Center Complex, Patiala, Punjab 147001',
+	name: 'Pizza hut',
+	address: ' Shop No. 25 A, City Center Complex, Patiala, Punjab',
 	location: 'Patiala',
 	category: 'Pizza',
 	vote: '4.4',
 	cuisines: 'Italian',
-	cost: '2200',
+	cost: '2100',
 	hours: '11 AM to 11 PM (Mon-Sun)',
  id :5,
+
  bestDish: {
 	name: 'Corn Pizza',
 	image: 'http://noblepig.com/images/2016/06/Avocado-and-Three-Bean-Salad-is-perfect-for-a-summertime-barbecue-side-dish.JPG'
           },
-	image: 'https://media.timeout.com/images/101564675/630/472/image.jpg'
-}]
+					image: 'https://media.timeout.com/images/101564675/630/472/image.jpg'
+				}]
 	$scope.restaurant = restaurants[$routeParams.id - 1];
 
 })
@@ -152,48 +153,48 @@ foodieApp.controller('mainController',function($scope) {
 },
 {
 	name: 'Dominos',
-	address: '12/24, Level 1, Block A , Big Bazzar, Baddi',
-	location: 'Baddi',
+	address: 'sector-18,Noida',
+	location: 'Noida',
 	category: 'Pizza',
 	vote: '4.7',
 	cuisines: 'Italian',
 	cost: '500',
 	hours: '12 Noon to 12 AM (Mon-Sun)',
  id :2,
-	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt_2XFEpZvUI_wAMAqEnnWoVr61jAej6k4VgzwRr-yONk2Es-h'
+	image: 'http://www.dominos.co.in/blog/wp-content/uploads/2011/08/03.jpg'
 },
 {
-	name: 'Silver Spoon Restaurant & Banquet Hall barnala',
-	address: 'White Wave Building, Opposite DC Complex, Handiaya Road, Barnala, Punjab 148101',
-	location: 'Connaught Place',
+	name: 'cheeze box',
+	address: 'near railway road ,kalka,haryana',
+	location: 'kalka',
 	category: 'Family Restaurant',
-	vote: '3.9',
+	vote: '4.0',
 	cuisines: 'Indian',
 	cost: '1000',
 	hours: '8 AM to 11 PM (Mon-Sun)',
  id:3,
-	image: 'http://lovethiscitytv.com/wp-content/uploads/2015/06/Top-25-New-Restaurants-in-Toronto2.jpeg'
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyD5HfeyitS3nIuzFsyrdpmLqYNckNrSaMJKWnonVvfbVoAW58MQ'
 },
 {
-	name: 'Pappu Dhaba',
-	address: 'Old City, Bathinda, Punjab 151001',
-	location: 'Bathinda',
+	name: 'saini Dhaba',
+	address: 'Old City,near ram leela ground,nalagarh',
+	location: 'nalagarh',
 	category: 'Casual Dining',
-	vote: '4.3',
+	vote: '2.5',
 	cuisines: 'Indian',
-	cost: '400',
+	cost: '300',
 	hours: '10 AM to 12 AM (Mon-Sun)',
  id :4,
-	image: 'http://restaurant.business.brookes.ac.uk/images/slideshow/restaurant.jpg'
+	image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFwZIQWY58NKGch1erq5Z82I_dnUzxeQgwAu63fjHXN1PUSW5VWw'
 },
 {
-	name: 'Pizza Nation',
-	address: ' Shop No. 25 A, City Center Complex, Patiala, Punjab 147001',
+	name: 'Pizza hut',
+	address: ' Shop No. 25 A, City Center Complex, Patiala, Punjab',
 	location: 'Patiala',
 	category: 'Pizza',
 	vote: '4.4',
 	cuisines: 'Italian',
-	cost: '2200',
+	cost: '2100',
 	hours: '11 AM to 11 PM (Mon-Sun)',
  id :5,
 	image: 'https://media.timeout.com/images/101564675/630/472/image.jpg'
